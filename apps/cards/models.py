360ae553773690a2ca99a4ac5b2fd129f958b1f5
mokom <mokomgeh45@gmail.com>
2021-10-03 14:12:35 -0400

    added django-filters

--- apps/cards/models.py
+++ apps/cards/models.py
@@ -6,6 +6,14 @@ class Card(models.Model):
     question = models.CharField(max_length=255)
     answer = models.TextField()
 
+    class QuestionTypes(models.IntegerChoices):
+        MCQs = 1
+        FILL_IN_BLANK = 2
+        TRUE_OR_FALSE = 3
+        SHORT_ANSWER = 4
+
+    question_type = models.IntegerField(choices=QuestionTypes.choices, default=1)
+
     deck = models.ForeignKey(Deck, on_delete=models.CASCADE, null=True, blank=True)
 
     def __str__(self):
