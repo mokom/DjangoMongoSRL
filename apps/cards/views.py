136b9b96f5a432702a82bb53b54d9c9c0410cb67
mokom <mokomgeh45@gmail.com>
2021-10-03 15:05:15 -0400

    complete project

--- apps/cards/views.py
+++ apps/cards/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from rest_framework import serializers, viewsets
+from django_filters import rest_framework as filters
+from rest_framework.filters import SearchFilter
 from .models import Card
 
 
@@ -12,3 +14,8 @@ class CardSerializer(serializers.ModelSerializer):
 class CardsViewSet(viewsets.ModelViewSet):
     serializer_class = CardSerializer
     queryset = Card.objects.all()
+
+    filter_backends = (filters.DjangoFilterBackend, SearchFilter)
+    filterset_fields = ('question_type',)
+    search_fields = ('question',)
+    ordering = ('question_type',)
